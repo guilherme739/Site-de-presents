57435e3acbf6de51a99490abddbd5349fec44ae7
Guilherme Buzo <buzoguilherme5@gmail.com>
2025-06-25 19:53:36 -0300

    Instalação e Configuração do Handlebars

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
+const { engine } = require ('express-handlebars');
+
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine','handlebars');
+app.set('views','./views');
+
+
+const conexao = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database:   'preset_db'
+});
+
+
+conexao.connect((erro) => {
+    if (erro)  {
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
+
 app.get("/", function(req, res){
-    res.write('Hello Word');
-    res.end();
+    res.render('index');
 });
 
 app.listen(8080);
+
 
